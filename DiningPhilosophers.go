422f616fa8f126f39585f16157b19aacfd03bc1a
ayman-anwar <122607921+ayman-anwar@users.noreply.github.com>
2023-08-26 11:37:24 +0530

    .

---
+++ DiningPhilosophers.go
@@ -0,0 +1,46 @@
+package main
+import(
+    "fmt"
+    "sync"
+    "time"
+)
+
+type Chopstick struct{
+    sync.Mutex
+}
+
+func main(){
+    var numPhilosophers int
+    
+    fmt.Print("Enter no. of philosophers: ")
+    fmt.Scan(&numPhilosophers)
+    
+    var wg sync.WaitGroup
+    chopsticks:=make([]Chopstick,numPhilosophers)
+    
+    for i:=0;i<numPhilosophers;i++{
+        wg.Add(1)
+        go func(id int){
+            defer wg.Done()
+            
+            fmt.Printf("Philosopher %d is thinking\n",id)
+            time.Sleep(time.Second)
+            left:=id
+            right:=(id+1)%numPhilosophers
+            
+            chopsticks[left].Lock()
+            chopsticks[right].Lock()
+            
+            fmt.Printf("Philosopher %d is eating\n",id)
+            time.Sleep(time.Second)
+            
+            chopsticks[left].Unlock()
+            chopsticks[right].Unlock()
+            time.Sleep(time.Second)
+            fmt.Printf("Philosopher %d finished eating\n",id)
+            
+            
+        }(i)
+    }
+    wg.Wait()
+}
