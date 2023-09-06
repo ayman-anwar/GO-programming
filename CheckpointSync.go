3c7ca7439622424e49add1aae020161b17e2fd69
ayman-anwar <122607921+ayman-anwar@users.noreply.github.com>
2023-09-06 09:24:35 +0530

    .

--- CheckpointSync.go
+++ CheckpointSync.go
@@ -6,26 +6,23 @@ import(
 )
 
 func main(){
-    var num int
     var wg sync.WaitGroup
     start:=make(chan struct{})
-    done:=make(chan struct{})
-    fmt.Print("Enter no. of workers:")
-    fmt.Scan(&num)
+    num:=5
     
     for i:=1;i<=num;i++{
         wg.Add(1)
         go func(id int){
             defer wg.Done()
-            
             <-start
             fmt.Printf("Worker %d is starting to work\n",id)
             time.Sleep(time.Second)
             
             fmt.Printf("Worker %d has reached checkpoint\n",id)
             time.Sleep(time.Second)
+            time.Sleep(time.Second)
             
-            fmt.Printf("Worker %d has completed the work\n",id)
+            fmt.Printf("Worker %d resuming the work\n",id)
             time.Sleep(time.Second)
             
             
@@ -34,7 +31,5 @@ func main(){
     fmt.Println("Starting workers.....")
     close(start)
     wg.Wait()
-    
-    close(done)
     fmt.Println("All workers completed their work")
 }
